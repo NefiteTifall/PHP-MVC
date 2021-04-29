7df43233fe0d361e75b3fabf453823c066c02cba
unknown <aoriol@edenschool.fr>
2021-04-29 16:52:32 +0200

    Ajout des routes en commentaire

--- public/index.php
+++ public/index.php
@@ -11,10 +11,19 @@ $router->get('/', "MainController@index");
 $router->get('/login/', "UserController@showLogin");
 $router->get('/register/', "UserController@showRegister");
 $router->get('/logout/', "UserController@logout");
+//$router->get('/eolienne', "placeholder@placeholder");
+//$router->get('/blog', "placeholder@placeholder");
+//$router->get('/article/:id', "placeholder@placeholder");
+//$router->get('/team', "placeholder@placeholder");
+//$router->get('/contact', "placeholder@placeholder");
+//$router->get('/cart', "placeholder@placeholder");
+//$router->get('/checkout', "placeholder@placeholder");
+
+
 $router->post('/valideUser/:username', "UserController@isUserNameValide");
-
-
 $router->post('/login/', "UserController@login");
 $router->post('/register/', "UserController@register");
+//$router->post('/addCart', "placeholder@placeholder");
+//$router->post('/checkout', "placeholder@placeholder");
 
 $router->run();
